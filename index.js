36e62603fc8871c79ad27ea06a3c7779584da169
Sam Avery <sam.avery1@gmail.com>
2022-08-29 19:02:29 -0500

    commit tree 66cf17dfe8480a9256333385543e9a4fc6c4ab53

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31;
+const num2 = 2; 
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()*10) +1;
+console.log(random)
+
+
+
+const num3 = 68
+const num4 = 8
+let mod = num3 % num4
+
+let max = Math.max(1,4,5,7,20);
